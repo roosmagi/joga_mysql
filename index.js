04e722d6e8daed7afa1d3c348a896ed0a7c6a113
Angela Roosmägi <angela.roosmagi@voco.ee>
2024-10-17 14:06:30 +0300

    Added handlebars template engine

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
+//application packages
 const express =require('express')
-const path = require('path')
-const mysql = require('mysql2')
-const bodyParser = require('body-parser')
-
 const app = express()
 
+const path = require('path')
+// add template engine
+const hbs = require('express-handlebars')
+// setup template engine directory and files extensions
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.engine('hbs', hbs.engine({
+    extname: 'hbs',
+    defaultLayout: 'main',
+    layoutsDir: __dirname+ '/views/layouts',
+}))
+
+const mysql = require('mysql2')
+
+const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended: true}))
 
-const con = mysql.createConnection({
+// create database connection
+var con = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'Qwerty',
